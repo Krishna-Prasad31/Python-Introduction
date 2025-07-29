3cf03f7b4c54c6fa7bcd48db27c248660eb0f9e8
Krishna prasad <krishnaprasad0707cr@gmail.com>
2025-07-29 22:27:36 +0530

    Addition program

---
+++ addition_machine.py
@@ -0,0 +1,8 @@
+name = input("Your name:")
+
+num_1 = input("Enter the first number you want to add")
+num_2 = input("Enter the second number you want to add")
+
+result = float(num_1) + float(num_2)
+
+print("Hello " + name + ", your numbers are: " + str(num_1) + ", " + str(num_2) + ". Your total sum is: " + str(result))
